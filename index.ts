b44499e144e653ae7e47dba2c7a478471e04fcc1
Michael de Checchi <mdechecchi@jahia.com>
2022-10-06 09:06:21 +0200

    Action initialization

---
+++ index.ts
@@ -0,0 +1,15 @@
+import TurndownService from "turndown";
+import * as fs from "fs";
+import core from "@actions/core";
+
+const htmlFile = core.getInput("html-file");
+console.log(htmlFile);
+
+const html = fs.readFileSync(htmlFile);
+
+// Create an instance of the Turndown service
+const turndownService = new TurndownService();
+const markdown = turndownService.turndown(html);
+console.log(markdown);
+
+core.setOutput("markdown-content", markdown);
